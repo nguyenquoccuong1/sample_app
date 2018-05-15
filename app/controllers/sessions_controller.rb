e6e02a04fc82524ef47d77bb3859092e90648ca2
qcuong <cuongnq161206@gmail.com>
2018-05-15 16:39:45 +0700

    Implement avanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      check_remember user
       redirect_to user
     else
       flash.now[:danger] = t("static_pages.home.mess_danger")
@@ -13,7 +14,11 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  def check_remember user
+    params[:session][:remember_me] == Settings.check_remember ? remember(user) : forget(user)
   end
 end
